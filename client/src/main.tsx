97f21b12f7bb7609613c1d78f2cbcbf01b6e37c1
Stopolis <designsteveh@gmail.com>
2026-02-28 14:44:24 -0800

    Wire /blog routes to BlogPage in main entrypoint

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import BlogPage from "./blog/BlogPage";
 import "./styles.css";
+
+const path = window.location.pathname.replace(/\/+$/, "");
+const isBlogRoute = path === "/blog" || path.startsWith("/blog/");
+const Root = isBlogRoute ? BlogPage : App;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
-
